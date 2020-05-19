ecaa658b68ae2e42757f660cabbd40675792f979
volodymyr.havryliuk <volodymyr.havryliuk@demicon.de>
2020-05-19 03:52:17 +0300

    Adds new features
    Add possibility to add, delete, upvote and downvote song.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,16 +1,11 @@
 import React, {Component} from 'react';
+import ReactDOM from "react-dom";
 import {withTracker} from 'meteor/react-meteor-data';
 import {Songs} from "../api/songs";
 import Song from "./Song";
 
+
 class App extends Component {
-    getSongs() {
-        return [
-            {_id: 1, title: 'Buddy Holly', artist: 'Weezer', votes: 10},
-            {_id: 2, title: 'Miss You', artist: 'The Rolling Stones', votes: 8},
-            {_id: 3, title: 'The Rising', artist: 'Bruce Springsteen', votes: 3},
-        ];
-    }
 
     renderSongs() {
         return this.props.songs.map((song) => (
@@ -18,11 +13,53 @@ class App extends Component {
         ));
     }
 
+    addSong(event) {
+        event.preventDefault();
+
+        console.log("Here");
+
+        // Find the text field via the React ref
+        const title = ReactDOM.findDOMNode(this.refs.title).value.trim();
+        const artist = ReactDOM.findDOMNode(this.refs.artist).value.trim();
+
+        Songs.insert({
+            title,
+            artist,
+            votes: 0,
+        });
+
+        // Clear form
+        ReactDOM.findDOMNode(this.refs.title).value = '';
+        ReactDOM.findDOMNode(this.refs.artist).value = '';
+    }
+
     render() {
         return (
             <div className="container">
                 <header>
                     <h1>Welcome to Songs Chart!</h1>
+                    <div className="row add-song">
+                        <div className="col-md-9 col-md-offset-3">
+                            Add new song
+                            <form className="form-inline" onSubmit={this.addSong.bind(this)}>
+                                <div className="form-group">
+                                    <label htmlFor="title">Title</label>
+                                    <input type="text" className="form-control" id="title" placeholder="Song title"
+                                           ref="title"/>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="Artist">Artist</label>
+                                    <input type="text" className="form-control" id="artist"
+                                           placeholder="Artist" ref="artist"/>
+                                </div>
+                                <button type="submit" className="btn btn-default">Add
+                                    song
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+
+
                 </header>
                 <div className="row">
                     {this.renderSongs()}
@@ -34,6 +71,6 @@ class App extends Component {
 
 export default withTracker(() => {
     return {
-        songs: Songs.find({}).fetch(),
+        songs: Songs.find({}, {sort: {votes: -1}}).fetch(),
     };
 })(App);
--- imports/ui/Song.jsx
+++ imports/ui/Song.jsx
@@ -1,14 +1,37 @@
 import React, {Component} from 'react';
+import {Songs} from "../api/songs";
 
 export default class Song extends Component {
+    deleteThisSong() {
+        Songs.remove(this.props.song._id);
+    }
+
+    upvoteThisSong() {
+        Songs.update(this.props.song._id, {
+            $set: {votes: this.props.song.votes + 1}
+        })
+    }
+
+    downvoteThisSong() {
+        Songs.update(this.props.song._id, {
+            $set: {votes: this.props.song.votes - 1}
+        })
+    }
+
     render() {
         return (
             <div className="col-xs-12">
                 <p className="bg-primary h-50 song">
                     <span> {`${this.props.song.title} - ${this.props.song.artist}.`}</span>
+                    <button type="button" className="close" aria-hidden="true"
+                            onClick={this.deleteThisSong.bind(this)}>&times;</button>
+                    <button type="button" className="btn btn-danger pull-right"
+                            onClick={this.downvoteThisSong.bind(this)}>Downvote
+                    </button>
                     <span className="badge pull-right">{this.props.song.votes}</span>
-                    <button type="button" className="btn btn-danger pull-right">Downvote</button>
-                    <button type="button" className="btn btn-success pull-right">Upvote</button>
+                    <button type="button" className="btn btn-success pull-right"
+                            onClick={this.upvoteThisSong.bind(this)}>Upvote
+                    </button>
                 </p>
             </div>
         );
